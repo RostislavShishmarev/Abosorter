8400a259fcd63a08b0b9bcdec165aa27635f5c66
RostislavShishmarev <shishmarev.r@mail.ru>
2024-12-10 12:22:51 +0300

    Start function of get options

--- main.c
+++ main.c
@@ -2,7 +2,20 @@
 #include "abonent/io.h"
 #include "sorts/sorts.h"
 
-int main() {
+int main(int argc, char **argv) {
+	int command = 0;
+	char *input_mode = NULL;
+	char *output_mode = NULL;
+
+	while ((command = getopt(argc, argv, "i:o:")) != -1) {
+		switch (command) {
+			case 'i':
+				input_mode = optarg;
+				break;
+			case 'o':
+				output_mode = optarg;
+				break;
+
 	return 0;
 }
 
