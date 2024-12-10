aaf16027d4cba61c0306952496c4a99fd0c31800
Ростислав <shishmarev.r@mail.ru>
2024-12-10 13:11:26 +0300

    Add get option function

--- main.c
+++ main.c
@@ -1,13 +1,25 @@
+#include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
 #include "abonent/abonent.h"
 #include "abonent/io.h"
 #include "sorts/sorts.h"
 
+#define OPTS "i:o:"
+#define BIN_MODE "bin"
+#define NOFILE_STRING "-"
+
 int main(int argc, char **argv) {
 	int command = 0;
 	char *input_mode = NULL;
+	char *input_filename = NULL;
 	char *output_mode = NULL;
+	char *output_filename = NULL;
 
-	while ((command = getopt(argc, argv, "i:o:")) != -1) {
+	while ((command = getopt(argc, argv, OPTS)) != -1) {
 		switch (command) {
 			case 'i':
 				input_mode = optarg;
@@ -15,6 +27,31 @@ int main(int argc, char **argv) {
 			case 'o':
 				output_mode = optarg;
 				break;
+			case '?':
+				if (index(OPTS, optopt) != NULL) {
+					fprintf(stderr, "Option -%c requires an argument\n", optopt);
+				} else if (isprint(optopt)) {
+					fprintf(stderr, "Unknown option `-%c`\n", optopt);
+				} else {
+					fprintf(stderr, "Unknown option character `\\x%x`\n", optopt);
+				}
+				return 1;
+			default:
+				abort();
+		}
+	}
+
+	if (argc >= optind + 1 && strcmp(argv[optind], NOFILE_STRING) != 0) {
+		input_filename = argv[optind];
+	}
+	if (argc >= optind + 2 && strcmp(argv[optind + 1], NOFILE_STRING) != 0) {
+		output_filename = argv[optind + 1];
+		
+	}	
+
+	printf("%s, %s\n", input_mode, output_mode);
+
+	printf("%s, %s\n", input_filename, output_filename);
 
 	return 0;
 }
